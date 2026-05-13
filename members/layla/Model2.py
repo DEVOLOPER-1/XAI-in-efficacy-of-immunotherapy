dccd92c493a1bb61632d18baad5190ca6eb0480d
LailaZayed <s-layla.zayed@zewailcity.edu.eg>
2026-05-13 04:29:20 +0200

    Updated model 2 code from multi task to images only

--- members/layla/Model2.py
+++ members/layla/Model2.py
@@ -406,150 +406,224 @@ class PatchEmbed(nn.Module):
         return flops
 
 
-class SwinTransformer_MulTasks(nn.Module):
-    r"""
-    Args:
-        img_size (int | tuple(int)): Input image size. Default 224
-        patch_size (int | tuple(int)): Patch size. Default: 4
-        in_chans (int): Number of input image channels. Default: 3
-        num_classes (int): Number of classes for classification head. Default: 1000
-        embed_dim (int): Patch embedding dimension. Default: 96
-        depths (tuple(int)): Depth of each Swin Transformer layer.
-        num_heads (tuple(int)): Number of attention heads in different layers.
-        window_size (int): Window size. Default: 7
-        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim. Default: 4
-        qkv_bias (bool): If True, add a learnable bias to query, key, value. Default: True
-        qk_scale (float): Override default qk scale of head_dim ** -0.5 if set. Default: None
-        drop_rate (float): Dropout rate. Default: 0
-        attn_drop_rate (float): Attention dropout rate. Default: 0
-    
-        drop_path_rate (float): Stochastic depth rate. Default: 0.1
-        norm_layer (nn.Module): Normalization layer. Default: nn.LayerNorm.
-        ape (bool): If True, add absolute position embedding to the patch embedding. Default: False
-        patch_norm (bool): If True, add normalization after patch embedding. Default: True
-        use_checkpoint (bool): Whether to use checkpointing to save memory. Default: False
-    """
+class SwinTransformerBinary(nn.Module):
 
-    def __init__(self, img_size=224, patch_size=4, in_chans=3, num_classes=1000, other_feature_num=0,
-                 embed_dim=96, depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24],
-                 window_size=7, mlp_ratio=4., qkv_bias=True, qk_scale=None,
-                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0.1,
-                 norm_layer=nn.LayerNorm, ape=False, patch_norm=True,
-                 use_checkpoint=False, **kwargs):
+    def __init__(
+        self,
+        img_size=224,
+        patch_size=4,
+        in_chans=3,
+        embed_dim=96,
+        depths=[2,2,2,2],
+        num_heads=[3,6,12,24],
+        window_size=7,
+        mlp_ratio=4.,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.1,
+        norm_layer=nn.LayerNorm,
+        ape=False,
+        patch_norm=True,
+        use_checkpoint=False
+    ):
+
         super().__init__()
 
-        self.num_classes = num_classes
-        # num_layers=4
         self.num_layers = len(depths)
-        self.embed_dim = embed_dim
-        self.ape = ape
-        self.patch_norm = patch_norm
-        self.num_features = int(embed_dim * 2 ** (self.num_layers - 1))
-        self.mlp_ratio = mlp_ratio
-        self.other_feature_num=other_feature_num
 
-        # split image into non-overlapping patches
+        self.embed_dim = embed_dim
+
+        self.ape = ape
+
+        self.patch_norm = patch_norm
+
+        self.num_features = int(
+            embed_dim * 2 ** (self.num_layers - 1)
+        )
+
+        self.mlp_ratio = mlp_ratio
+
+        # Patch Embedding
         self.patch_embed = PatchEmbed(
-            img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim,
-            norm_layer=norm_layer if self.patch_norm else None)
-        # num_patches = 56*56 = 3136
+            img_size=img_size,
+            patch_size=patch_size,
+            in_chans=in_chans,
+            embed_dim=embed_dim,
+            norm_layer=norm_layer if patch_norm else None
+        )
+
         num_patches = self.patch_embed.num_patches
-        # patches_resolution=[56,56]
+
         patches_resolution = self.patch_embed.patches_resolution
+
         self.patches_resolution = patches_resolution
 
-        # absolute position embedding
+        # Absolute Position Embedding
         if self.ape:
-            # absolute_pos_embed = torch.size(1, patches_resolution[0] * patches_resolution[1], 96)
-            self.absolute_pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim))
-            trunc_normal_(self.absolute_pos_embed, std=.02)
 
-        self.pos_drop = nn.Dropout(p=drop_rate)
+            self.absolute_pos_embed = nn.Parameter(
+                torch.zeros(1, num_patches, embed_dim)
+            )
 
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
+            trunc_normal_(
+                self.absolute_pos_embed,
+                std=.02
+            )
 
-        # build layers
+        self.pos_drop = nn.Dropout(
+            p=drop_rate
+        )
+
+        # Stochastic depth
+        dpr = [
+            x.item()
+            for x in torch.linspace(
+                0,
+                drop_path_rate,
+                sum(depths)
+            )
+        ]
+
+        # Build Swin Layers
         self.layers = nn.ModuleList()
-        # self.num_layers = len(depths) = 4
+
         for i_layer in range(self.num_layers):
-            layer = BasicLayer(dim=int(embed_dim * 2 ** i_layer),
-                               input_resolution=(patches_resolution[0] // (2 ** i_layer),
-                                                 patches_resolution[1] // (2 ** i_layer)),
-                               depth=depths[i_layer],
-                               num_heads=num_heads[i_layer],
-                               window_size=window_size,
-                               mlp_ratio=self.mlp_ratio,
-                               qkv_bias=qkv_bias, qk_scale=qk_scale,
-                               drop=drop_rate, attn_drop=attn_drop_rate,
-                               drop_path=dpr[sum(depths[:i_layer]):sum(depths[:i_layer + 1])],
-                               norm_layer=norm_layer,
-                               downsample=PatchMerging if (i_layer < self.num_layers - 1) else None,
-                               use_checkpoint=use_checkpoint)
+
+            layer = BasicLayer(
+                dim=int(embed_dim * 2 ** i_layer),
+
+                input_resolution=(
+                    patches_resolution[0] // (2 ** i_layer),
+                    patches_resolution[1] // (2 ** i_layer)
+                ),
+
+                depth=depths[i_layer],
+
+                num_heads=num_heads[i_layer],
+
+                window_size=window_size,
+
+                mlp_ratio=self.mlp_ratio,
+
+                qkv_bias=qkv_bias,
+
+                qk_scale=qk_scale,
+
+                drop=drop_rate,
+
+                attn_drop=attn_drop_rate,
+
+                drop_path=dpr[
+                    sum(depths[:i_layer]):
+                    sum(depths[:i_layer + 1])
+                ],
+
+                norm_layer=norm_layer,
+
+                downsample=PatchMerging
+                if (i_layer < self.num_layers - 1)
+                else None,
+
+                use_checkpoint=use_checkpoint
+            )
+
             self.layers.append(layer)
 
-        self.norm = norm_layer(self.num_features)
-        
+        # Final normalization
+        self.norm = norm_layer(
+            self.num_features
+        )
+
+        # Global average pooling
         self.avgpool = nn.AdaptiveAvgPool1d(1)
-        self.head_sub1 = nn.Linear(self.num_features+self.other_feature_num, 1) if num_classes > 0 else nn.Identity()
-        self.head_sub2 = nn.Linear(self.num_features+self.other_feature_num, 1) if num_classes > 0 else nn.Identity()
-        self.head_main = nn.Linear(self.num_features+self.other_feature_num+2, 1) if num_classes > 0 else nn.Identity()
 
-        self.sigmoid=nn.Sigmoid()
+        # Binary classification head
+        self.head = nn.Linear(
+            self.num_features,
+            1
+        )
 
+        # Initialize weights
         self.apply(self._init_weights)
 
     def _init_weights(self, m):
+
         if isinstance(m, nn.Linear):
+
             trunc_normal_(m.weight, std=.02)
-            if isinstance(m, nn.Linear) and m.bias is not None:
+
+            if m.bias is not None:
                 nn.init.constant_(m.bias, 0)
+
         elif isinstance(m, nn.LayerNorm):
+
             nn.init.constant_(m.bias, 0)
+
             nn.init.constant_(m.weight, 1.0)
 
     @torch.jit.ignore
     def no_weight_decay(self):
+
         return {'absolute_pos_embed'}
 
     @torch.jit.ignore
     def no_weight_decay_keywords(self):
+
         return {'relative_position_bias_table'}
 
     def forward_features(self, x):
+
+        # Patch embedding
         x = self.patch_embed(x)
-        
+
+        # Add position embedding
         if self.ape:
             x = x + self.absolute_pos_embed
+
         x = self.pos_drop(x)
 
+        # Swin Transformer layers
         for layer in self.layers:
             x = layer(x)
 
-        x = self.norm(x)  # B L C
-        x = self.avgpool(x.transpose(1, 2))  # B C 1
+        # Final normalization
+        x = self.norm(x)
+
+        # Global pooling
+        x = self.avgpool(
+            x.transpose(1, 2)
+        )
+
         x = torch.flatten(x, 1)
+
         return x
 
-    def forward(self, x, tabdata=None):
+    def forward(self, x):
+
         x = self.forward_features(x)
 
-        if tabdata != None:
-            x=torch.cat((x,tabdata),dim=-1)
+        x = self.head(x)
 
-        pred_sub1 = self.sigmoid(self.head_sub1(x))
-        pred_sub2 = self.sigmoid(self.head_sub2(x))
-
-        x=torch.cat((x,pred_sub1,pred_sub2),dim=-1)
-
-        pred_main = self.sigmoid(self.head_main(x))
-
-        return pred_main.squeeze(), pred_sub1.squeeze(), pred_sub2.squeeze()
+        return x.squeeze(1)
 
     def flops(self):
+
         flops = 0
+
         flops += self.patch_embed.flops()
-        for i, layer in enumerate(self.layers):
+
+        for layer in self.layers:
             flops += layer.flops()
-        flops += self.num_features * self.patches_resolution[0] * self.patches_resolution[1] // (2 ** self.num_layers)
-        flops += self.num_features * self.num_classes
+
+        flops += (
+            self.num_features
+            * self.patches_resolution[0]
+            * self.patches_resolution[1]
+            // (2 ** self.num_layers)
+        )
+
+        flops += self.num_features
+
         return flops
